e91a0e8d858957f290bc5dfafd67e7375825f819
Pedro Augusto Alexander <pedroaugustodev4@gmail.com>
2025-03-22 10:51:01 -0300

    Delete app directory

--- app/src/main/java/task/app/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package task.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import task.app.models.Card;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-    
-}
